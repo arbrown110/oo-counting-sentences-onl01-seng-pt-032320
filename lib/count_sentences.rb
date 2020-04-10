f11e3fb565ee704fc3abbdba1316b0058a084901
chic-caller-7520 <arbrown110@gmail.com>
2020-04-10 01:53:03 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,25 +3,6 @@ require 'pry'
 class String
 
   def sentence?
-    
-  end
-
-  def question?
-
-  end
-
-  def exclamation?
-
-  end
-
-  def count_sentences
-
-  end
-end
-
-class String
-
-  def sentence?
     self.end_with?(".")
   end
 
